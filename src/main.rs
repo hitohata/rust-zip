d67cf4c2ae57072636d10e3f9ab899a7899fe3f5
hitohata <hirohatatro@gmail.com>
2025-02-20 18:53:52 -0800

    updated

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,50 @@ use std::{
     fs::File,
     io::{Read, Write},
 };
-use zip::{self, write::SimpleFileOptions};
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use zip::{self, unstable::write, write::SimpleFileOptions};
 
 fn main() {
-    let current_dir = std::env::current_dir().unwrap();
-
     let dir_name = "./upload-file-zip";
-    let file_name = format!("{dir_name}/fuga.txt");
     let key_name = "existing.zip";
-    std::fs::create_dir(dir_name).unwrap();
-    let mut file = std::fs::File::create(file_name.to_owned()).unwrap();
-    file.write_all(b"Hello, world!").unwrap();
+    std::fs::create_dir_all(dir_name).unwrap();
 
+    let zip_file = std::fs::OpenOptions::new().read(true).write(true).create(true).truncate(true).open(key_name).unwrap();
+    let zip_writer = zip::ZipWriter::new(zip_file);
+
+    let zip = Arc::new(Mutex::new(zip_writer));
+
+    for i in ["a", "b", "c", "d"] {
+        write_file(i, zip.clone());
+    }
+
+    if let Ok(mutex) = Arc::try_unwrap(zip) {
+        let value = mutex.into_inner().unwrap();
+        value.finish().unwrap();
+    } else {
+        panic!("error")
+    };
+
+}
+
+fn write_file(file_name: &str, zip: Arc<Mutex<zip::ZipWriter<File>>>) {
     let options = SimpleFileOptions::default()
         .compression_method(zip::CompressionMethod::Bzip2)
         .unix_permissions(0o755);
 
-    let mut buffer: Vec<u8> = Vec::new();
+    let contents = format!("Contents, {}", file_name);
 
-    let dist_path = std::path::Path::new(&file_name);
-    let zip_file = std::fs::File::create(key_name).unwrap();
-    let mut zip = zip::ZipWriter::new(zip_file);
+    let buffer = contents.as_bytes();
 
-    zip.start_file("upload-file-zip/fuga.txt", options).unwrap();
+    // let mut zip = zip::ZipWriter::new_append(zip_file).unwrap();
 
-    let mut f = std::fs::File::open(file_name).unwrap();
-    f.read_to_end(&mut buffer).unwrap();
-    zip.write_all(&buffer).unwrap();
-    buffer.clear();
+    let mut zip = zip.lock().unwrap();
 
-    zip.finish().unwrap();
+    zip.start_file(file_name, options)
+        .unwrap();
+
+    println!("{:?}: {:?}", file_name, &buffer);
+
+    zip.write_all(buffer).unwrap();
 }
